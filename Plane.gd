7e4a756521bfd2b5cf0491c2df54d9ab72eccdf7
Christodoulou Andreas <koc.clicky@gmail.com>
2021-12-30 21:43:20 +0200

    minor updates

--- Plane.gd
+++ Plane.gd
@@ -1,14 +1,20 @@
 extends KinematicBody2D
 
+onready var blue_missile = preload("res://PlaneMissile.tscn")
+onready var missile_pos = $MissilePosition
+
+
 var velocity = Vector2.ZERO
+var is_plane_shoot = false
 
 export (int) var plane_speed = 550
-export (int) var plane_accelaration = 550
+export (int) var plane_accelaration = 1000
 export (int) var plane_friction = 20
 
 func _physics_process(delta: float) -> void:
 	plane_move(delta)
-	
+	if Input.is_action_just_pressed("ui_right"):
+		plane_shoot(delta)
 	velocity = move_and_slide(velocity)
 	
 
@@ -19,4 +25,9 @@ func plane_move(delta):
 	if player_input.y != 0:
 		velocity = velocity.move_toward(player_input * plane_speed, plane_accelaration * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, plane_friction)
+		velocity = velocity.move_toward(Vector2.ZERO, plane_friction)	
+	
+func plane_shoot(delta):
+	var plane_blue_missile = blue_missile.instance()
+	plane_blue_missile.global_position = missile_pos.global_position
+	get_tree().root.add_child(plane_blue_missile)
