a1b0faf5a10772feaac0dc96727a9975e4cab716
Christodoulou Andreas <koc.clicky@gmail.com>
2022-01-08 20:18:50 +0200

    spaceship changes

--- Planet1.gd
+++ Planet1.gd
@@ -1,1 +1,5 @@
 extends "res://planets.gd"
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free()
--- planets.gd
+++ planets.gd
@@ -4,7 +4,7 @@ var rng = RandomNumberGenerator.new()
 
 func _init() -> void:
 	rng.randomize()
-	var random_velocity = rng.randf_range(100, 600)
+	var random_velocity = rng.randf_range(300, 600)
 	print(random_velocity)
 	linear_velocity.x = -random_velocity
 
