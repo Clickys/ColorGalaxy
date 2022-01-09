0d8ed10af2de42a3e0ebae10345e584e1a0ea8d9
Christodoulou Andreas <koc.clicky@gmail.com>
2022-01-09 02:17:40 +0200

    entry scene

--- EntryScene.gd
+++ EntryScene.gd
@@ -1,12 +1,25 @@
-extends Node2D
+extends Control
 
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
+onready var mouse_btn_red = $Red
+onready var mouse_btn_blue = $Blue
+onready var mouse_btn_green = $Green
+onready var animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var new_dialog = Dialogic.start('entry_scene') 
 	get_tree().current_scene.add_child(new_dialog)
+	mouse_btn_red.grab_focus()
+
+func _process(delta: float) -> void:
+	var focus_btn = get_focus_owner()
+	if Input.is_action_just_pressed("ui_accept"):
+		mouse_btn_red.grab_focus()
+	if focus_btn.name == "Red":
+		animation_player.play("AnimateRed")
+	elif focus_btn.name == "Green":
+		animation_player.play("AnimateGreen")
+	else:
+		animation_player.play("AnimateBlue")
+	
+		
